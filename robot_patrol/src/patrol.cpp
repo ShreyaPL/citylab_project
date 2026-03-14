455f7c49028d868b77ec76dfc7811505d2a954b8
ShreyaPL <shreyap.lanjewar@gmail.com>
2026-03-14 20:20:43 +0000

    Implement patrolling algorithm to compute turn angle

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -1,16 +1,21 @@
+#include "geometry_msgs/msg/detail/twist__struct.hpp"
 #include <functional>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
+#include <geometry_msgs/msg/twist.hpp>
 #include <cmath>
 
 class Patrol : public rclcpp::Node{
 public:
     Patrol()
-    : Node("node_name")
+    : Node("node_name"), linear_(0.1), direction_(0.0)
     {
         scan_subscriber_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
             "/fastbot_1/scan", 10, 
             &Patrol::scan_callback, this, std::placeholders::_1);
+        
+        //publisher object
+        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
     }
 
 private:
@@ -21,13 +26,63 @@ private:
         // Using only the front 180 degrees: [-pi/2, pi/2]
         double front_left = M_PI/2.0f;
         double front_right = -M_PI/2.0f;
+        double front = 0.0;
 
-        int start_index = (front_right - scan_.angle_min) / scan_.angle_increment;
-        int end_index = (front_left - scan_.angle_min) / scan_.angle_increment;
+        int start_index = static_cast<int>(front_right - scan_.angle_min) / scan_.angle_increment;
+        int end_index = static_cast<int>(front_left - scan_.angle_min) / scan_.angle_increment;
+        int front_index = static_cast<int>(front - scan_.angle_min) / scan_.angle_increment;
+        RCLCPP_INFO(this->get_logger(), "Front index: %d", front_index);
 
+        double distance_front = scan_.ranges[front_index];
+        // check if distance_front is valid value
+        if(std::isnan(distance_front) || distance_front < scan_.range_min)
+        { 
+            stop_robot();
+            return;
+        }
+        if(std::isinf(distance_front) || distance_front > scan_.range_max)
+        {
+            distance_front = scan_.range_max;
+        }
+
+        if (distance_front > 0.35)
+        {
+            // move forward
+            direction_ = 0.0;
+            publish_msg();
+        }
+        else
+        {
+            // get largest distance ray
+            double max_distance = scan_.range_min;
+            int max_index;
+            for(int i = start_index; i <= end_index; i++)
+            {
+                if(scan_.ranges[i] > max_distance){
+                    max_distance = scan_.ranges[i];
+                    max_index = i;
+                }
+            }
+            // angle from index
+            direction_ = scan_.angle_min + (max_index * scan_.angle_increment);
+            RCLCPP_INFO(this->get_logger(), "Direction of maximum distance: %0.2f", direction_);
+
+            publish_msg();
+        }
+    }
+
+    void publish_msg(){
+        geometry_msgs::msg::Twist msg;
+        msg.linear.x = linear_;
+        msg.angular.z = direction_ / 2.0f;
+        publisher_.publish(msg);
     }
 
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_subscriber_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
 
     sensor_msgs::msg::LaserScan scan_;
+    
+    double linear_;
+    double direction_;
 };
