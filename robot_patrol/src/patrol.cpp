25a23263256ff14c683ef4f5feea6a993001b10d
ShreyaPL <shreyap.lanjewar@gmail.com>
2026-03-14 19:24:18 +0000

    Updated valid names from laser_scan to scan. Added front 180 degrees indices.

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -1,22 +1,33 @@
-#include "rclcpp/node.hpp"
-#include "sensor_msgs/msg/detail/laser_scan__struct.hpp"
 #include <functional>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
+#include <cmath>
 
 class Patrol : public rclcpp::Node{
 public:
     Patrol()
     : Node("node_name")
     {
-        laserscan_subscriber_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+        scan_subscriber_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
             "/fastbot_1/scan", 10, 
-            &Patrol::laserscan_callback, this, std::placeholders::_1);
+            &Patrol::scan_callback, this, std::placeholders::_1);
     }
 
 private:
-    void laserscan_callback(const sensor_msgs::msg::LaserScan::SharedPtr & msg)
-    {}
+    void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr & msg)
+    {
+        scan_ = msg;
 
-    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laserscan_subscriber_;
+        // Using only the front 180 degrees: [-pi/2, pi/2]
+        double front_left = M_PI/2.0f;
+        double front_right = -M_PI/2.0f;
+
+        int start_index = (front_right - scan_.angle_min) / scan_.angle_increment;
+        int end_index = (front_left - scan_.angle_min) / scan_.angle_increment;
+
+    }
+
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_subscriber_;
+
+    sensor_msgs::msg::LaserScan scan_;
 };
